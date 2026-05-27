207b8a606c4ba0c416d0b8888fcc08a3ea79f478
balchaparshu <balchaparshu@gmail.com>
2026-05-27 21:11:39 +0530

    feat: complete bdh model implementation and training scripts pipeline

--- implementation/fix_tokenizer.py
+++
@@ -1,5 +0,0 @@
-with open('bbpe_tokenizer.py', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-# Replace line 55 (index 54) - fix the special_tokens list
-lines[54] = '            special_tokens = ["[PAD]", "[UNK]", "[CLS]", "[SEP]", "
